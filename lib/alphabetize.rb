b129c9342a1e0bd9bfd2eec9d78751075f4fa817
w.h.sinn <w.h.sinn@gmail.com>
2019-03-27 05:07:51 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,5 @@
-def alphabetize(arr)
-  # code here
+ESPERANTO_ALPHABET = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+def alphabetize(array)
+  array.sort_by { |a| a.split("").map{ |char| ESPERANTO_ALPHABET.index(char) } }
 end
